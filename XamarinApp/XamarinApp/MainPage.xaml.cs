7c2bd2de7a3442f0b4c4f763929f0e16b719bf68
Shi Hao Wang <st890409@hotmail.com>
2017-12-26 16:38:14 +0800

    Initial commit

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp
+{
+    /*
+     * 將預設繼承 ContentPage 修改為 PlatformDefaultPaddingPage
+     * 
+     * 並需要加工 MainPage.xaml 檔案的主要 XAML Element
+     * 請參考 MainPage.xaml 註解內容
+     */
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : PlatformDefaultPaddingPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ XamarinApp/XamarinApp/ProviderPage/PlatformDefaultPaddingPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    /// <summary>
+    /// 已設定 iOS 預設上內距離基底 ContentPage 物件
+    /// </summary>
+    public abstract class PlatformDefaultPaddingPage : ContentPage
+    {
+        private readonly double _default_padding_top = 20;
+
+        protected PlatformDefaultPaddingPage()
+        {
+            this.SetIOSDefaultPadding();
+        }
+
+        /// <summary>
+        /// 設定 iOS 裝置的預設 Padding 上內距離
+        /// </summary>
+        private void SetIOSDefaultPadding()
+        {
+            switch(Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    this.Padding = new Thickness(0, _default_padding_top, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
